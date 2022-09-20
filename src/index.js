8cd51f40005f02d748e76faf783ff8a9d224d29c
sanket-kumbhare <sanketkumbhare1511@gmail.com>
2022-09-21 00:39:23 +0530

    BrowserRouter basename prop added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </React.StrictMode>
